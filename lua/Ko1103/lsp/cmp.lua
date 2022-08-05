37ce24ca7b06001f8a089776ee0e4d0decf95940
山浦功 <ko.foot1103@gmail.com>
2022-08-05 19:02:16 +0900

    :sparkles: add terraform

--- lua/Ko1103/lsp/cmp.lua
+++ lua/Ko1103/lsp/cmp.lua
@@ -1,9 +1,9 @@
-local status_ok, cmp = pcall(require, 'cmp')
+local status_ok, cmp = pcall(require, "cmp")
 if not status_ok then
 	return
 end
 
--- vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
 cmp.setup({
 	snippet = {
@@ -17,24 +17,24 @@ cmp.setup({
 		documentation = cmp.config.window.bordered(),
 	},
 	mapping = cmp.mapping.preset.insert({
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	}),
 	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
+		{ name = "nvim_lsp" },
 		-- { name = 'luasnip' }, -- For luasnip users.
-		{ name = 'vsnip' },
+		{ name = "vsnip" },
 	}, {
-		{ name = 'buffer' },
-	})
+		{ name = "buffer" },
+	}),
 })
 
-cmp.setup.cmdline('/', {
+cmp.setup.cmdline("/", {
 	mapping = cmp.mapping.preset.cmdline(),
 	sources = {
-		{ name = 'buffer' }
-	}
+		{ name = "buffer" },
+	},
 })
--- lua/Ko1103/lsp/null-ls.lua
+++ lua/Ko1103/lsp/null-ls.lua
@@ -1,32 +1,25 @@
-local status_ok, null_ls = pcall(require, 'null-ls')
+local status_ok, null_ls = pcall(require, "null-ls")
 if not status_ok then
 	return
 end
 
--- local lua_source = {
--- 	require("null-ls").builtins.diagnostics.stylua,
--- 	require("null-ls").builtins.formatting.stylua,
--- 	filetypes = { "lua" }
--- }
---
 local b = null_ls.builtins
-
 local with_root_file = function(builtin, file)
-	return builtin.with {
+	return builtin.with({
 		condition = function(utils)
 			return utils.root_has_file(file)
 		end,
-	}
+	})
 end
 
 local sources = {
 	--formattin
 	b.formatting.prettierd,
 	with_root_file(b.formatting.stylua, "stylua.toml"),
+	b.formatting.terraform_fmt,
 
 	-- diagnostics
 	b.diagnostics.eslint,
-	b.diagnostics.stylua,
 
 	-- code actions
 	b.code_actions.eslint_d,
@@ -36,29 +29,7 @@ local sources = {
 }
 
 null_ls.setup({
-	debug = false,
+	debug = true,
 	sources = sources,
 })
 
-
-
-
-
--- null_ls.setup({
--- 	debug = false,
--- 	sources = {
--- 		-- for lua
--- 		lua_source,
---
--- 		-- for javascript and typescript
--- 		require("null-ls").builtins.diagnostics.eslint,
--- 		require("null-ls").builtins.code_actions.eslint_d,
--- 		-- require("null-ls").builtins.formatting.prettierd.with({
--- 		-- 	condition = function(utils)
--- 		-- 		return utils.has_file { ".prettierrc", ".prettierrc.js" }
--- 		-- 	end,
--- 		-- }),
--- 		require("null-ls").builtins.formatting.prettierd,
--- 		require("null-ls").builtins.completion.spell,
--- 	},
--- })
