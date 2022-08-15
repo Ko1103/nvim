f2eae68e99531fe1b40287984fb6cef2aec62709
山浦功 <ko.foot1103@gmail.com>
2022-08-15 10:41:26 +0900

    :dash: format init.lua

--- lua/Ko1103/init.lua
+++ lua/Ko1103/init.lua
@@ -1,14 +1,14 @@
-require ('Ko1103.lsp')
-require ('Ko1103.colorscheme')
-require ('Ko1103.comment')
-require ('Ko1103.gitsigns')
-require ('Ko1103.indent_blankline')
-require ('Ko1103.keymaps')
-require ('Ko1103.lualine')
-require ('Ko1103.nvim-tree')
-require ('Ko1103.treesitter')
-require ('Ko1103.telescope')
-require ('Ko1103.project') -- load after telescope
+require("Ko1103.lsp")
+require("Ko1103.colorscheme")
+require("Ko1103.comment")
+require("Ko1103.gitsigns")
+require("Ko1103.indent_blankline")
+require("Ko1103.keymaps")
+require("Ko1103.lualine")
+require("Ko1103.nvim-tree")
+require("Ko1103.treesitter")
+require("Ko1103.telescope")
+require("Ko1103.project") -- load after telescope
 
 -- global setting
-require ('Ko1103.options')
+require("Ko1103.options")
